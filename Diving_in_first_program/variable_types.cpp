d364db309d35ac8594d6c590b4590f83fe84d8ea
Fairbuyng <emijere.richard@gmail.com>
2022-10-09 12:45:12 +0100

    Progressing

--- Diving_in_first_program/variable_types.cpp
+++ Diving_in_first_program/variable_types.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 using namespace std;
 
+constexpr double Div_Expr(double a, double b)
+{
+    return a / b;
+}
+
+void AreaOfCircle()
+{
+    // const double pi = 22.0 / 7;
+
+    constexpr double pi = Div_Expr(22, 7);
+
+    typedef unsigned int Strictly_Positive_Integer;
+    Strictly_Positive_Integer numEggsInBasket = 80;
+
+    int r;
+    cout << "Enter the radius of the circle: ";
+    cin >> r;
+
+    cout << "The area of the circle is " << pi * r * r << " cm2" << endl;
+}
+
+void AutoKeyword()
+{
+    auto coinFlippedHeads = true;
+    auto largeNum = 250'000'000'000'000;
+
+    cout << "coinFlippedHeads = " << coinFlippedHeads << endl;
+    cout << "largeNum = " << largeNum << ",";
+    cout << "sizeof(largeNum) = " << sizeof(largeNum) << endl;
+}
+
 int main()
 {
     unsigned short uShortValue = 65535;
@@ -11,6 +42,9 @@ int main()
     cout << "signed short 32767 + 1 = ";
     cout << ++signedShort << endl;
     cout << sizeof(long int) << endl;
+
+    // AutoKeyword();
+    AreaOfCircle();
 
     return 0;
 }
