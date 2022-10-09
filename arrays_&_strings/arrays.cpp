6d802a40eb890d0a7374fb72c259b22fadb09f3f
Fairbuyng <emijere.richard@gmail.com>
2022-10-09 12:45:38 +0100

    Arrays and strings

---
+++ arrays_&_strings/arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myNum[5] = {34,
+                    35,
+                    46,
+                    99};
+
+    cout << myNum[2] << endl;
+    return 0;
+}
